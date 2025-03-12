06d50fec1cad0fd395deb81b1797483fb88920c0
bbachdev <bbach7139@gmail.com>
2025-03-12 13:08:37 -0500

    Add example icon to Sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,7 +1,9 @@
+import { FaHome } from "react-icons/fa";
+
 export default function Sidebar() {
   return (
-    <div className={`px-2 bg-cyan-800 text-white`}>
-      Sidebar
+    <div className={`pt-10 flex flex-col items-center w-16 px-2 bg-cyan-800 text-white`}>
+      <FaHome className={`w-6 h-6`} />
     </div>
   )
 }
