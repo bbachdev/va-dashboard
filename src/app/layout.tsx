17f31724a1d9af4013ae4e4efb1eed1d2d0eb102
bbachdev <bbach7139@gmail.com>
2025-03-12 12:48:28 -0500

    Move layout components to own folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import Sidebar from '@/components/Sidebar';
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+import Sidebar from '@/components/layout/Sidebar';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
