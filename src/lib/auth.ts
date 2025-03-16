171afd4ffb857ce0ae3a866545f6a0624ab308cb
bbachdev <bbach7139@gmail.com>
2025-03-16 11:19:40 -0500

    Add 2-Factor plugin (BetterAuth)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,19 @@
 import { betterAuth } from "better-auth";
 import { NeonDialect } from "kysely-neon"
+import { twoFactor } from "better-auth/plugins"
 
 const dialect = new NeonDialect({
   connectionString: process.env.POSTGRES_DB_URL
 })
  
 export const auth = betterAuth({
+    appName: "VA Dashboard",
     database: {
       dialect,
       type: "postgres"
     },
     emailAndPassword: {  
       enabled: true
-    }
+    },
+    plugins: [twoFactor()]
 })
